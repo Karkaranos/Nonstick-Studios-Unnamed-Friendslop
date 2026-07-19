59d932bdc18db3e5b7149d7103c56eb9c2ff9752
Toby <104741505+TheBeesSneeze@users.noreply.github.com>
2026-07-19 12:59:22 -0700

    Manager

--- Operation P.H.I.S.H. Prototype/Assets/Toby Temp/Tether/TetherManager.cs
+++ Operation P.H.I.S.H. Prototype/Assets/Toby Temp/Tether/TetherManager.cs
@@ -1,28 +1,16 @@
 /*****************************************************************************
-* File Name :         TetherSegment.cs
+* File Name :         TetherManager.cs
 * Author :            Toby Schamberger
-* Creation Date :     7/18/26
+* Creation Date :     7/19/26
 *
-* Brief Description : Refers to one piece of the entire tether. 
-*                     Recursively references other TetherSegment in a node-like structure.
-* 
-*                     Specifically this script is to be placed on the node before whatever comes next.
-*                     (IE. The hatch of the ship, before the player, or just somewhere random in the middle of the whole thing).
+* Brief Description : 
 *****************************************************************************/
 
+using NaughtyAttributes;
 using UnityEngine;
 
 public class TetherManager : Singleton<TetherManager>   
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [BoxGroup("Tether Settings")]
+    public float MaxLengthToCreateNewTetherSegment = 15;
 }
