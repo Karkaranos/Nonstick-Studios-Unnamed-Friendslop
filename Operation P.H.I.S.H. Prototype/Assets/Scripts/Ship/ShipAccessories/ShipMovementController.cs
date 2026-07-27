bfeda6d9c894019dca42f67dd6c458729649c2b4
jayishembryo <kembry@mail.bradley.edu>
2026-07-27 16:47:19 -0500

    Ship Movement WIP (BLOCKED)

---
+++ Operation P.H.I.S.H. Prototype/Assets/Scripts/Ship/ShipAccessories/ShipMovementController.cs
@@ -0,0 +1,77 @@
+/*************************************************
+Author Names : 		    Jay Embry
+Date Created : 		    7/27/2026
+Date Last Modified : 	7/27/2026
+Brief Description : 	Controls the movement of the P.H.I.S.H.
+External Resources : 	
+***************************************************/
+using System.Collections;
+using System.Threading;
+using NaughtyAttributes;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+using static ShipMovementControllers;
+
+public class ShipMovementControllers : MonoBehaviour
+{
+    #region VARIABLES
+
+    public enum ControllerType
+    {
+        FBLever,
+        ADLever,
+        Wheel,
+    }
+
+    bool moving;
+
+    [Tooltip("Match this variable with the game object!")]
+    [SerializeField] ControllerType controllerType;
+
+
+    [Space(10)]
+    [ShowIf(nameof(controllerType), ControllerType.FBLever), SerializeField]
+    float FBAdjustmentRate = 0;
+    [ShowIf(nameof(controllerType), ControllerType.FBLever), SerializeField]
+    float FBAdjustmentCap;
+
+
+    [Space(10)]
+    [ShowIf(nameof(controllerType), ControllerType.ADLever), SerializeField]
+    float ADAdjustmentCap = 0;
+    [ShowIf(nameof(controllerType), ControllerType.ADLever), SerializeField]
+    float ADAdjustmentRate;
+
+
+    [Space(10)]
+    [ShowIf(nameof(controllerType), ControllerType.Wheel), SerializeField]
+    float wheelAdjustmentRate = 0;
+
+    #endregion VARIABLES
+
+    /// <summary>
+    /// should be called upon interacting with lever
+    /// </summary>
+    public void InteractWithController()
+    {
+        ShipMovement.Instance.Moving = !moving;
+        moving = !moving;
+
+        if (moving)
+        {
+            //TODO: disable player movement
+        }
+        else
+        {
+            //TODO: enable player movement
+        }
+    }
+
+    //TODO: put controls here!!!!!
+    //tl;dr: when the player uses WASD while moving,
+    //it should adjust whichever value is associated with controllerType
+    //then, it should gradually adjust the associated variable in ShipMovement using mathf.lerp(?)
+    //to keep the coroutine in ShipMovement from running constantly,
+    //it should start whenever the variable in this script is less than or greater than 0,
+    //and stop whenever the variable in ShipMovement is 0
+}
---
+++ Operation P.H.I.S.H. Prototype/Assets/Scripts/Ship/ShipMovement.cs
@@ -0,0 +1,92 @@
+/*************************************************
+Author Names : 		    Jay Embry
+Date Created : 		    7/24/2026
+Date Last Modified : 	7/27/2026
+Brief Description : 	Moves the P.H.I.S.H.
+External Resources : 	
+***************************************************/
+
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class ShipMovement : Singleton<ShipMovement>
+{
+    #region VARIABLES
+
+    [HideInInspector] public bool Moving;
+
+    [Tooltip("The speed of the P.H.I.S.H.")]
+    [SerializeField] float forwardBackwardMovementSpeed;
+
+    [Tooltip("The ascension speed of the P.H.I.S.H.")]
+    [SerializeField] float upDownMovementSpeed;
+
+    [Tooltip("The rotation speed of the P.H.I.S.H.")]
+    [SerializeField] float leftRightMovementSpeed;
+
+    [HideInInspector] public float FBAdjustment;
+    [HideInInspector] public float ADAdjustment;
+    [HideInInspector] public float WheelAdjustment;
+
+    #endregion VARIABLES
+
+    /// <summary>
+    /// starts one of the coroutines that allows the ship to move
+    /// </summary>
+    /// <param name="controllerType">
+    /// in which way the p.h.i.s.h. is moving\
+    /// the int corresponds with the enum entry in ShipMovementController
+    /// </param>
+    public void StartMoving(int controllerType)
+    {
+        switch (controllerType)
+        {
+            case 0:
+                StartCoroutine(MovingForwardAndBackward());
+                break;
+
+            case 1:
+                StartCoroutine(MovingUpAndDown());
+                break;
+
+            case 2:
+                StartCoroutine(MovingLeftOrRight());
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    [Button]
+    IEnumerator MovingForwardAndBackward()
+    {
+        while(FBAdjustment != 0 || Moving)
+        {
+            transform.Translate(Vector3.forward * forwardBackwardMovementSpeed * FBAdjustment * Time.deltaTime);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+    [Button]
+    IEnumerator MovingUpAndDown()
+    {
+        while (ADAdjustment != 0 || Moving)
+        {
+            transform.Translate(Vector3.up * upDownMovementSpeed * ADAdjustment * Time.deltaTime);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+    [Button]
+    IEnumerator MovingLeftOrRight()
+    {
+        while (WheelAdjustment != 0 || Moving)
+        {
+            transform.Rotate(Vector3.up * leftRightMovementSpeed * WheelAdjustment * Time.deltaTime);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
