d8189bdab0451a2abd2e4259ca8d4d128dd550a5
jayishembryo <kembry@mail.bradley.edu>
2026-08-13 11:56:28 -0500

    yeah!

---
+++ Operation P.H.I.S.H. Prototype/Assets/Scripts/ALCHEMY SLOP/Ingredients/IngredientInteractable.cs
@@ -0,0 +1,41 @@
+/*************************************************
+Author Names : 		    Jay Embry
+Date Created : 		    08/13/2026
+Brief Description : 	Script for interacting with ingredients
+                        Thank you for IInteractable, Cade!!
+External Resources :    	
+***************************************************/
+
+using UnityEngine;
+
+public class IngredientInteractable : MonoBehaviour, IInteractable
+{
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    //wait im blocked
+    public void EnterInteract(PlayerController pc)
+    {
+        rb.isKinematic = true;
+        transform.parent = pc.PickupPoint;
+    }
+
+    public void ExitInteract()
+    {
+
+    }
+
+    public void EnterHover()
+    {
+
+    }
+
+    public void ExitHover()
+    {
+
+    }
+}
