df90691567b06b2d48fa2739f7260aa19b4e57e6
Cade Naylor <arnaylor@mail.bradley.edu>
2026-08-16 16:36:34 -0500

    Canvas spawns on orb interact

---
+++ Operation P.H.I.S.H. Prototype/Assets/Scripts/Interactions/OpenCanvasInteractable.cs
@@ -0,0 +1,48 @@
+/*************************************************
+Author Names : 		    Cade Naylor
+Date Created : 		    08/16/2026
+Brief Description : 	Instantiates a canvas when interacted with, if it hasn't opened a canvas
+                        Destroys the canvas on interact end
+External Resources :    	
+***************************************************/
+using UnityEngine;
+
+public class OpenCanvasInteractable : MonoBehaviour, IAlchemyInteractable
+{
+    [SerializeField, Tooltip("The canvas that spawns when this object is interacted with")] private GameObject canvasToSpawn;
+    private GameObject openedCanvas = null;
+    public void EnterHover()
+    {
+        return;
+    }
+
+    public void EnterInteract(AlchemyPlayerController apc)
+    {
+        if(openedCanvas == null)
+        {
+            openedCanvas = Instantiate(canvasToSpawn, apc.transform);
+        }
+        else
+        {
+            Debug.Log("Canvas is already open");
+        }
+    }
+
+    public void ExitHover()
+    {
+        return;
+    }
+
+    public void ExitInteract()
+    {
+        if(openedCanvas != null)
+        {
+            Destroy(openedCanvas);
+            openedCanvas = null;
+        }
+        else
+        {
+            Debug.Log("No canvas to destroy");
+        }
+    }
+}
